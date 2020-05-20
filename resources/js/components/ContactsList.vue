277fb35824a0da3ac8089c72c1a637a9b36d5fca
Gabriel <gabriel.gressie@gmail.com>
2020-05-20 23:08:25 +0200

    made a few changes

--- resources/js/components/ContactsList.vue
+++ resources/js/components/ContactsList.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="contacts-list">
         <ul>
-            <li v-for="contact in contacts" :key="contact.id">
+            <li v-for="contact in contacts" :key="contact.id" @click="selectContact(index, contact)" :class="{ 'selected': index === selected }">
                 <div class="avatar">
                     <img :src="contact.profile_image" :alt="contact.name">
                 </div>
@@ -20,6 +20,18 @@
             contacts: {
                 type: Array,
                 default: []
+            }
+        },
+        data() {
+            return {
+                selected: 0
+            };
+        },
+        methods: {
+            selectContact(index, contact) {
+                this.selected = index;
+
+                this.$emit('selected', contact);
             }
         }
     }
